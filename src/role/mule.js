5974787123123e33e5c85723fdafa413c114c207
Orien Vandenbergh <orien@icebergh.us>
2020-10-06 15:14:34 -0400

    Limit maximum mule size

--- src/role/mule.js
+++ src/role/mule.js
@@ -1,21 +1,20 @@
 var Utils = require('lib.utils');
 
 function design(maxEnergy) {
-  const baseCost  = (1 * BODYPART_COST[WORK])+(1 * BODYPART_COST[CARRY])+(1 * BODYPART_COST[MOVE]);
-  const unitCost  = (1 * BODYPART_COST[CARRY])+(1 * BODYPART_COST[MOVE]);
-  let avlEnergy   = maxEnergy - baseCost;
-  let units       = Math.max(0, Math.floor(avlEnergy / unitCost));
-  let workBits    = 0;
-  let carrBits    = units
-  let moveBits    = units + (Math.floor((avlEnergy - (unitCost * units)) / BODYPART_COST[MOVE]));
+  const baseCost  = BODYPART_COST[CARRY] + BODYPART_COST[MOVE] + BODYPART_COST[WORK];
+  const unitCost  = BODYPART_COST[CARRY] + BODYPART_COST[MOVE];
+  const avlEnergy = maxEnergy - baseCost;
+  const units     = Math.min(14, Math.max(0, Math.floor(avlEnergy / unitCost)));
+  const carrBits  = units;
+  const moveBits  = units + Math.min(1, Math.floor((avlEnergy - (unitCost * units)) / BODYPART_COST[MOVE]));
   return [MOVE, CARRY, WORK].concat(new Array(carrBits).fill(CARRY)).concat(new Array(moveBits).fill(MOVE)).sort();
 }
 
-var roleMule = {
-  spawn: function(base, inheritance) {
+const roleMule = {
+  spawn(base, inheritance) {
     const spawn   = Game.spawns[base];
     const energy  = spawn.room.energyAvailable;
-    const newName = Utils.nameCreep('mule',base);
+    const newName = Utils.nameCreep('mule', base);
     if (!inheritance || !inheritance.design) {
       console.log('Missing Inheritance!');
       if (!inheritance) {
@@ -24,7 +23,7 @@ var roleMule = {
 
       inheritance.design = design(energy);
     }
-    
+
     // eslint-disable-next-line
     const spawnCost = inheritance.design.reduce((s,e) => s+BODYPART_COST[e], 0);
     const newMemory = {role: 'mule', container: inheritance.container, design: inheritance.design};
@@ -34,17 +33,17 @@ var roleMule = {
       return spawn.spawnCreep(inheritance.design, newName, {memory: newMemory});
     }
 
-    const tempDesign = design(energy);
-    console.log('Spawning Mule[' + energy + ']: ' + newName + '\ntemplate: ' + JSON.stringify(tempDesign) +
+    const tDesign = design(energy);
+    console.log('Spawning Mule[' + energy + ']: ' + newName + '\ntemplate: ' + JSON.stringify(tDesign) +
       '\nmemory: ' + JSON.stringify(newMemory));
-    return spawn.spawnCreep(tempDesign, newName, {memory: newMemory});
+    return spawn.spawnCreep(tDesign, newName, {memory: newMemory});
   },
   /** @param {Creep} creep **/
-  run: function(creep) {
+  run(creep) {
     if (!creep.memory.storage) {
       creep.memory.storage = creep.room.find(FIND_STRUCTURES, {
         filter: structure => {
-          return (structure.structureType === STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+          return (structure.structureType === STRUCTURE_STORAGE);
         }
       })[0].id;
     }
@@ -74,24 +73,23 @@ var roleMule = {
       }
     } else {
       const drops = creep.pos.findClosestByPath(creep.room.find(FIND_DROPPED_RESOURCES), {
-//          filter: (d) => { return (d.resourceType == RESOURCE_ENERGY) }
+          // filter: (d) => { return (d.resourceType == RESOURCE_ENERGY) }
       });
       const container = Game.getObjectById(creep.memory.container);
       if (drops) {
         if (creep.pickup(drops) === ERR_NOT_IN_RANGE) {
           creep.moveTo(drops,  {visualizePathStyle: {stroke: '#ffaa00'}});
         }
+
         if (creep.pos.getRangeTo(container) <= 1) {
           creep.withdraw(container, RESOURCE_ENERGY);
           creep.withdraw(container, RESOURCE_KEANIUM);
         }
+      } else if (creep.pos.getRangeTo(container) <= 1) {
+        creep.withdraw(container, RESOURCE_ENERGY);
+        creep.withdraw(container, RESOURCE_KEANIUM);
       } else {
-        if (creep.pos.getRangeTo(container) <= 1) {
-          creep.withdraw(container, RESOURCE_ENERGY);
-          creep.withdraw(container, RESOURCE_KEANIUM);
-        } else {
-          creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
-        }
+        creep.moveTo(container, {visualizePathStyle: {stroke: '#ffaa00'}});
       }
     }
   }
